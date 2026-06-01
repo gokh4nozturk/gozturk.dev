bfb1c60e70abda4d7f80d6af683ac3c369eb15a6
gokhanozturk <gokhaanozturk@gmail.com>
2026-06-02 01:03:14 +0300

    feat: implement "Now" block and HelloAnimation component

--- app/page.jsx
+++ app/page.jsx
@@ -1,107 +1,13 @@
-"use client";
-
 import About from "@components/About";
-import {
-  AppleHelloEnglishEffect,
-  AppleHelloFrenchEffect,
-  AppleHelloJapaneseEffect,
-  AppleHelloRussianEffect,
-  AppleHelloSpanishEffect,
-  AppleHelloTurkishEffect,
-  AppleHelloVietnameseEffect,
-} from "@components/apple-hello";
-import { cn } from "@lib/utils";
-import { AnimatePresence, motion } from "motion/react";
-import { useState } from "react";
+import HelloAnimation from "@components/HelloAnimation";
+import Now from "@components/Now";
 
 export default function Home() {
-  const [onViewHello, setOnViewHello] = useState("english");
   return (
     <div className="flex h-full flex-col justify-between sm:relative">
       <About />
-
-      <div className="flex h-full items-start justify-center">
-        <div
-          className={cn(
-            "-translate-x-1/2 -translate-y-2/3 fixed top-2/3 left-1/2 w-min overflow-hidden sm:h-32",
-            "grid place-items-center",
-          )}
-        >
-          <AnimatePresence mode="popLayout">
-            <motion.div
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              initial={{ opacity: 0 }}
-              key="english"
-              transition={{ delay: 0.1, duration: 0.2, ease: "easeInOut" }}
-            >
-              {onViewHello === "english" && (
-                <AppleHelloEnglishEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("turkish");
-                    }, 800);
-                  }}
-                />
-              )}
-              {onViewHello === "turkish" && (
-                <AppleHelloTurkishEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("french");
-                    }, 800);
-                  }}
-                />
-              )}
-              {onViewHello === "french" && (
-                <AppleHelloFrenchEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("spanish");
-                    }, 800);
-                  }}
-                />
-              )}
-              {onViewHello === "spanish" && (
-                <AppleHelloSpanishEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("vietnamese");
-                    }, 800);
-                  }}
-                />
-              )}
-              {onViewHello === "vietnamese" && (
-                <AppleHelloVietnameseEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("russian");
-                    }, 800);
-                  }}
-                />
-              )}
-              {onViewHello === "russian" && (
-                <AppleHelloRussianEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("japanese");
-                    }, 800);
-                  }}
-                />
-              )}
-              {onViewHello === "japanese" && (
-                <AppleHelloJapaneseEffect
-                  onAnimationComplete={() => {
-                    setTimeout(() => {
-                      setOnViewHello("english");
-                    }, 800);
-                  }}
-                />
-              )}
-            </motion.div>
-          </AnimatePresence>
-        </div>
-      </div>
+      <HelloAnimation />
+      <Now />
     </div>
   );
 }
---
+++ components/HelloAnimation.jsx
@@ -0,0 +1,88 @@
+"use client";
+
+import {
+  AppleHelloEnglishEffect,
+  AppleHelloFrenchEffect,
+  AppleHelloJapaneseEffect,
+  AppleHelloRussianEffect,
+  AppleHelloSpanishEffect,
+  AppleHelloTurkishEffect,
+  AppleHelloVietnameseEffect,
+} from "@components/apple-hello";
+import { cn } from "@lib/utils";
+import { AnimatePresence, motion } from "motion/react";
+import { useState } from "react";
+
+export default function HelloAnimation() {
+  const [onViewHello, setOnViewHello] = useState("english");
+  return (
+    <div className="flex h-full items-start justify-center">
+      <div
+        className={cn(
+          "-translate-x-1/2 -translate-y-2/3 fixed top-2/3 left-1/2 w-min overflow-hidden sm:h-32",
+          "grid place-items-center",
+        )}
+      >
+        <AnimatePresence mode="popLayout">
+          <motion.div
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            initial={{ opacity: 0 }}
+            key="english"
+            transition={{ delay: 0.1, duration: 0.2, ease: "easeInOut" }}
+          >
+            {onViewHello === "english" && (
+              <AppleHelloEnglishEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("turkish"), 800);
+                }}
+              />
+            )}
+            {onViewHello === "turkish" && (
+              <AppleHelloTurkishEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("french"), 800);
+                }}
+              />
+            )}
+            {onViewHello === "french" && (
+              <AppleHelloFrenchEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("spanish"), 800);
+                }}
+              />
+            )}
+            {onViewHello === "spanish" && (
+              <AppleHelloSpanishEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("vietnamese"), 800);
+                }}
+              />
+            )}
+            {onViewHello === "vietnamese" && (
+              <AppleHelloVietnameseEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("russian"), 800);
+                }}
+              />
+            )}
+            {onViewHello === "russian" && (
+              <AppleHelloRussianEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("japanese"), 800);
+                }}
+              />
+            )}
+            {onViewHello === "japanese" && (
+              <AppleHelloJapaneseEffect
+                onAnimationComplete={() => {
+                  setTimeout(() => setOnViewHello("english"), 800);
+                }}
+              />
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </div>
+  );
+}
